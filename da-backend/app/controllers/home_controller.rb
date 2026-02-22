82bb2f2d1e5db58a4483d0414faaa99fccafc7b3
violetbrowne <conorb+violet@firstpartners.net>
2026-02-22 05:27:29 +0000

    Creation pages for actions and credentials

--- da-backend/app/controllers/home_controller.rb
+++ da-backend/app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index; end
   def quickstart; end
   def actions; end
-  def new_connector; end
+  def actions_library; end
+  def credentials; end
 end
