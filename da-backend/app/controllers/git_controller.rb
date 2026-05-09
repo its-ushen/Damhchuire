c63faf8b3f24b0dd870779375ae536385d8efbdf
violetbrownie <violetbfreelancer@gmail.com>
2026-05-09 18:44:48 -0400

    Refactor git controller

--- da-backend/app/controllers/git_controller.rb
+++ da-backend/app/controllers/git_controller.rb
@@ -2,28 +2,13 @@ require "zlib"
 require "digest"
 
 class GitController < ActionController::API
-  _file_contents = "hey hi:)"
-  FILE_CONTENTS = "blob #{_file_contents.length}\x00#{_file_contents}"
-  FILE_HASH = Digest::SHA1.hexdigest FILE_CONTENTS\
-
-  _tree_contents = "100644 filey\x00#{FILE_HASH.scan(/../).map { |x| x.hex.chr }.join}"
-  TREE_CONTENTS = "tree #{_tree_contents.length}\x00#{_tree_contents}"
-  TREE_HASH = Digest::SHA1.hexdigest TREE_CONTENTS
-
-  _HEAD_CONTENT = <<-TEXT
-tree #{TREE_HASH}
-author chord <chord@chord.ie> 1777585149 -0400
-committer chord <chord@chord.ie> 1777585149 -0400
-
-Hey hi!
-TEXT
-
-  HEAD_CONTENT = "commit #{_HEAD_CONTENT.length}\x00#{_HEAD_CONTENT}"
-
-  HEAD_HASH = Digest::SHA1.hexdigest HEAD_CONTENT
+  DEMO_FILES = { "filey" => "contenty" }
 
   def info_refs
-    render plain: "#{HEAD_HASH}\trefs/heads/main\r\n"
+    hash_map = Git::calculate_git_hashes(DEMO_FILES)
+    head_hash = hash_map["HEAD"]
+
+    render plain: "#{head_hash}\trefs/heads/main\r\n"
   end
 
   def head
@@ -31,21 +16,13 @@ TEXT
   end
 
   def objects
-    Rails.logger.info(HEAD_HASH)
-    Rails.logger.info(TREE_HASH)
-    Rails.logger.info(FILE_HASH)
+    hash_map = Git::calculate_git_hashes(DEMO_FILES)
+    total_hash = params[:start_hex] + params[:end_hex]
 
-    case params[:start_hex]
-    when HEAD_HASH[0..1]
-      contents = HEAD_CONTENT
-    when TREE_HASH[0..1]
-      contents = TREE_CONTENTS
-    when FILE_HASH[0..1]
-      contents = FILE_CONTENTS
-    else
+    if !total_hash.in?(hash_map)
       raise "unknown hash"
     end
 
-    send_data Zlib::Deflate.deflate(contents)
+    send_data Zlib::Deflate.deflate(hash_map[total_hash])
   end
 end
